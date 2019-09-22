23b13901c63f40d0b1f6911f53a20f63baf47d5e
Van-Hoang Nguyen <vanguyen@paypal.com>
2019-09-22 18:28:06 +0800

    Add stance task to glue

--- examples/utils_glue.py
+++ examples/utils_glue.py
@@ -88,6 +88,37 @@ class DataProcessor(object):
             return lines
 
 
+class StanceProcessor(DataProcessor):
+    """Processor for the sentence pair classification data sets"""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train.tsv")))
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_labels(self):
+        """See base class."""
+        return ["support", "deny", "comment", "unrelated"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, line[0])
+            text_a = line[1]
+            text_b = line[2]
+            label = line[3]
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+        return examples
+
+
 class MrpcProcessor(DataProcessor):
     """Processor for the MRPC data set (GLUE version)."""
 
@@ -561,6 +592,8 @@ def compute_metrics(task_name, preds, labels):
         return {"acc": simple_accuracy(preds, labels)}
     elif task_name == "mrpc":
         return acc_and_f1(preds, labels)
+    elif task_name == "stance":
+        return acc_and_f1(preds, labels)
     elif task_name == "sts-b":
         return pearson_and_spearman(preds, labels)
     elif task_name == "qqp":
@@ -589,6 +622,7 @@ processors = {
     "qnli": QnliProcessor,
     "rte": RteProcessor,
     "wnli": WnliProcessor,
+    "stance": StanceProcessor
 }
 
 output_modes = {
@@ -602,6 +636,7 @@ output_modes = {
     "qnli": "classification",
     "rte": "classification",
     "wnli": "classification",
+    "stance": "classification"
 }
 
 GLUE_TASKS_NUM_LABELS = {
@@ -614,4 +649,5 @@ GLUE_TASKS_NUM_LABELS = {
     "qnli": 2,
     "rte": 2,
     "wnli": 2,
+    "stance": 4
 }
---
+++ finetune_stance.sh
@@ -0,0 +1,16 @@
+export GLUE_DIR=GLUE
+export TASK_NAME=STANCE
+
+python ./examples/run_glue.py \
+  --model_type xlnet \
+  --model_name_or_path xlnet-base-cased \
+  --task_name $TASK_NAME \
+  --do_train \
+  --do_eval \
+  --do_lower_case \
+  --data_dir $GLUE_DIR/$TASK_NAME \
+  --max_seq_length 128 \
+  --per_gpu_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3.0 \
+  --output_dir /tmp/$TASK_NAME/
