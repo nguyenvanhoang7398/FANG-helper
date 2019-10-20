dd4df033677bd80f95fa9cbd72325141b56d5261
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2019-10-20 22:10:27 +0800

    Fix task name

--- infer_sst.sh
+++ infer_sst.sh
@@ -1,9 +1,11 @@
 export GLUE_DIR=GLUE
-export TASK_NAME=STANCE
+export TASK_NAME=SST-2
 
 python ./examples/run_glue.py \
   --model_type roberta \
   --model_name_or_path SENTIMENT_FNN/roberta/ \
   --task_name $TASK_NAME \
   --do_predict \
-  --do_lower_case
+  --do_lower_case \
+  --data_dir $GLUE_DIR/SENTIMENT_FNN \
+  --output_dir SENTIMENT_FNN/roberta/
