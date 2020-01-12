4540a46af176dc2e9ce27d284de0b6c75dada47d
Van-Hoang Nguyen <vanguyen@paypal.com>
2020-01-12 19:26:30 +0700

    Update infer and test stance

--- test_stance.sh
+++ test_stance.sh
@@ -3,10 +3,10 @@ export TASK_NAME=STANCE2
 
 python ./examples/run_glue.py \
   --model_type roberta \
-  --model_name_or_path STANCE2/roberta/ \
+  --model_name_or_path STANCE2/roberta-large/ \
   --task_name $TASK_NAME \
-  --do_predict \
+  --do_eval \
   --max_seq_length 100 \
   --do_lower_case \
   --data_dir $GLUE_DIR/$TASK_NAME \
-  --output_dir $TASK_NAME/roberta/
+  --output_dir $TASK_NAME/roberta-large/
